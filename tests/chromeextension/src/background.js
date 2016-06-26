c5c24f7881f080dd67d3737dda618f9491a44ada
Elias <elias@youiest.com>
2016-06-26 13:46:13 +0200

    coffee comment in js

--- tests/chromeextension/src/background.js
+++ tests/chromeextension/src/background.js
@@ -1,9 +1,9 @@
 oneSteps = function ( url ) {
   // we simply need to know what page we were on when ctrl t or ctrl n was pressed
   if ( url == "chrome://newtab/" ) {
-    #console.log( "chrome://newtab/" );
+    //console.log( "chrome://newtab/" );
   } else {
-    #console.log( url );
+    //console.log( url );
     chrome.storage.sync.set( {
       'lastStep': url
     } )
@@ -18,8 +18,8 @@ chrome.tabs.onHighlighted.addListener( function ( highlightInfo ) {
 
 chrome.windows.onFocusChanged.addListener( function ( windowId ) {
   if ( windowId == 'WINDOW_ID_NONE' ) {
-    #console.log( windowId, 'WINDOW_ID_NONE' );
+    //console.log( windowId, 'WINDOW_ID_NONE' );
   } else {
-    #console.log( windowId );
+    //console.log( windowId );
   }
 } )
