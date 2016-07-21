d0dac79f0f0f505bec53a2bab7690542d92dd851
Elias <elias@youiest.com>
2016-07-21 13:17:19 +0200

    got title

--- tests/chromeextension/src/background.js
+++ tests/chromeextension/src/background.js
@@ -1,18 +1,25 @@
-oneSteps = function ( url ) {
+oneSteps = function ( url ,title) {
   // we simply need to know what page we were on when ctrl t or ctrl n was pressed
   if ( url == "chrome://newtab/" ) {
     //console.log( "chrome://newtab/" );
   } else {
     //console.log( url );
     chrome.storage.sync.set( {
-      'lastStep': url
+      'lastStep': url,
+      'last': {
+          'url': url,
+          'title': title
+      }
     } )
   }
 }
 
 chrome.tabs.onHighlighted.addListener( function ( highlightInfo ) {
   chrome.tabs.get( highlightInfo.tabIds[ 0 ], function ( tab ) {
-    oneSteps( tab.url )
+    //var title = document.getElementsByTagName("title")[0].innerHTML;
+    //console.log(title);
+    //console.log(tab.title);
+    oneSteps( tab.url , tab.title)
   } )
 } );
 
