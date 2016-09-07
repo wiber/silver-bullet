32b07853f251f4d5982a91632803f00ff8d4eae2
Elias <elias@youiest.com>
2016-09-07 12:47:53 +0400

    fix title injection bug..

--- tests/chromeextension/src/newtab/newtab.js
+++ tests/chromeextension/src/newtab/newtab.js
@@ -1,12 +1,14 @@
 websiteURL = "http://localhost:3000/"
 chrome.storage.sync.get("last", function(lastPlace) {
-  lastPlace =  lastPlace.last
- console.log(lastPlace,lastPlace.url,lastPlace.title);
+    lastPlace = lastPlace.last
+    console.log(lastPlace, lastPlace.url, lastPlace.title);
+    // must handle hashbangs
+    // http://localhost:3000/about?from=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F%23inbox&lastTitle=Inbox%20(4)%20-%20eliasmoosman%40gmail.com%20-%20Gmail
+    lastPlace.url.replace('0','+').replace('#','+')
     encoded = encodeURIComponent(lastPlace.url)
-    encodedTitle = encodeURIComponent(lastPlace.title)
-   console.log(encoded,' is where from ',decodeURIComponent(encoded),' lastStep', lastPlace.lastStep);
-        // step is is called on newtab too, so where were we again?
-        //console.log(websiteURL+"about?from="+encoded);
+    encodedTitle = lastPlace.title.replace('0','+').replace('#','+').replace('(','+').replace(')','+')
+    console.log("lastPlace transformed", lastPlace,encoded.replace('0','+').replace('#','+'));
+    console.log(encoded, ' is where from ', decodeURIComponent(encoded), ' lastStep', lastPlace.title, encodedTitle);
     iFrame = document.getElementById('linkstateframe')
-    iFrame.src = websiteURL + "about?from=" + encoded+ "&lastTitle=" + encodedTitle
+    iFrame.src = websiteURL + "about?from=" + encoded + "&lastTitle=" + encodedTitle
 });
