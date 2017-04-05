dd4447c6b5a7cd8ac4c2752f19d7e02422ffce3f
wiber <elias@youiest.com>
2017-04-05 06:10:43 +0400

    Breaks certain urls for flow router to work, flow router can't handle () in urls

--- linkstate/tests/chromeextension/src/newtab/newtab.js
+++ linkstate/tests/chromeextension/src/newtab/newtab.js
@@ -9,7 +9,7 @@ frameit = function(lastPlace) {
         for (var i in obj) {
             if (obj.hasOwnProperty(i)) {
                 // flowrouter-ssr cannot handle '()' which often appears in titles
-                spec = obj[i]//.replace('(', '+').replace(')', '+')
+                spec = obj[i].replace(/[\-_.!~*'()]/g,"_")//.replace('(', '+').replace(')', '+')
                 console.log(spec);
                 parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(spec));
             }
@@ -18,7 +18,7 @@ frameit = function(lastPlace) {
     }
     queryParams = {}
     queryParams.from = lastPlace.url
-    queryParams.lastTitle = lastPlace.title.replace(/[^a-zA-Z0-9\s\-_.!~*'()]/g,"_")
+    queryParams.lastTitle = lastPlace.title//.replace(/[\-_.!~*'()]/g,"_")
     qp = toQueryString(queryParams)
     src = websiteURL + 'about?' + qp
     console.log("queryParams", queryParams, src);
