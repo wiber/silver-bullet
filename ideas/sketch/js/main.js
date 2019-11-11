607a5b09dc63f8e20a375b1c16f63b5a3c3e7476
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-11-11 17:22:31 +0530

    createBox work

--- ideas/sketch/js/main.js
+++ ideas/sketch/js/main.js
@@ -16,102 +16,48 @@ $(function () {
         data = f
     });
     $('.tabs').tabs();
-    $(".emoji_icon").click(function () {
-        var temp = $(this).text()
-        $(".emoji").html(temp);
-        $(".emoji").addClass("active");
-    });
+    // $(".emoji_icon").click(function () {
+    //     var temp = $(this).text()
+    //     $(".emoji").html(temp);
+    //     $(".emoji").addClass("active");
+    // });
 
     $(".title").text(data.title);
     $("#modal_header").val(data.title);
 
-    $.each(userData, function(i, f) {
-        var temp_id = f._id
-        var img_icon = f.screenshot 
-        $.each(f.memorable, function(i, f) {
-            $("#memorable_ul").append('<li class="collection-item record_id">'+ temp_id +'<p class="add_memorable" id='+ temp_id +'> '+ f.memorable +' </p><p class="createdBy_name">'+ f.createdBy +' </p><p class="createdAt_date" id="date'+temp_id+'">'+ f.createdAt +'</p></li>');
-        });
-        $.each(f.comment, function(i, f) {
-            $("#comment_ul").append('<li class="collection-item avatar"><img src="'+ img_icon +'" alt="" class="circle"><span class="creater_name">'+ f.comment +'</span><p class="created_data">'+ f.createdBy +' </br> '+ f.createdAt +'</p></li>');
-        });
-    });
-
-    // $.each(userData, function (i, f) {
-    //     $.each(f.memorable, function (i, f) {
-    //         console.log("userData", f);
-    //         $("#card_container").append('<div class="card-content white-text"><div class="row"><div class="col s3" style="margin-top: 5px;"><i class="medium material-icons emoji brand-logo">sentiment_dissatisfied</i>'+
-    //          +' </div>'+
-    //          +'<div class="col s6">'+
-    //          +'<h4 class="grey-text text-darken-4 title center-align card-title truncate"></h4>'+
-    //          +'</div>'+
-    //          +'<div class="col s3" style="margin-top: 10px;">'+
-    //          +' <a href="#modal" id="modal_btn"'+
-    //          +'class="secondary-content  btn-floating waves-effect waves-light modal-trigger">'+
-    //          +'<i class="small material-icons">edit</i>'+
-    //          +'</a>'+
-    //          +'              </div>'+
-    //          +'</div>'+
-    //          +'<div class="row">'+
-    //          +'<div class="col s5"></div>'+
-    //          +'<div class="col s2">'+
-    //          +'<img src="images/img_avatar.jpg" alt="" class="circle responsive-img">'+
-    //          +'</div>'+
-    //          +'<div class="col s5"></div>'+
-    //          +'</div>'+
-    //          +'<div class="row valign-wrapper">'+
-    //          +'<div class="col s2">'+
-    //          +'<img src="images/img_avatar.jpg" alt="" class="circle responsive-img">'+
-    //          +'</div>'+
-    //          +'<div class="col s8 center-align">'+
-    //          +'<img src="images/img_avatar.jpg" alt="" class="circle responsive-img">'+
-    //          +'</div>'+
-    //          +'<div class="col s2">'+
-    //          +'<img src="images/img_avatar.jpg" alt="" class="circle responsive-img">'+
-    //          +'</div>'+
-    //          +'</div>'+
-    //          +'<div class="row">'+
-    //          +'<div class="col s5"></div>'+
-    //          +'<div class="col s2">'+
-    //          +'<img src="images/img_avatar.jpg" alt="" class="circle responsive-img">'+
-    //          +'</div>'+
-    //          +'<div class="col s5"></div>'+
-    //          +'</div>'+
-    //          +'<div class="row">'+
-    //          +'<div class="card-tabs">'+
-    //          +'<ul class="tabs tabs-fixed-width">'+
-    //             +'<li class="tab">'+
-    //             +'<a href="#memorable_div" class="blue white-text">Memorable</a>'+
-    //             +'</li>'+
-    //             +'<li class="tab">'+
-    //             +'<a href="#comment_div" class="blue white-text">Comment</a>'+
-    //             +'</li>'+
-    //             +'</ul>'+
-    //           +'</div>'+
-    //           +'<div class="card-content black-text">'+
-    //             +'<div id="memorable_div">'+
-    //             +'<ul class="collection" id="memorable_ul"></ul>'+
-    //               +'</div>'+
-    //             +'<div id="comment_div">'+
-    //             +'<ul class="collection" id="comment_ul"></ul>'+
-    //               +'<div class="input-field">'+
-    //               +'<i class="material-icons prefix send_comment">mode_edit</i>'+
-    //                 +'<textarea id="comment_txt" class="materialize-textarea"></textarea>'+
-    //                 +'</div>'+
-    //               +'</div>'+
-    //             +'  </div>'+
-    //         +'</div>'+
-    //         +' </div>')
+    // $.each(userData, function(i, f) {
+    //     var temp_id = f._id
+    //     var img_icon = f.screenshot 
+    //     $.each(f.memorable, function(i, f) {
+    //         $("#memorable_ul").append('<li class="collection-item record_id">'+ temp_id +'<p class="add_memorable" id='+ temp_id +'> '+ f.memorable +' </p><p class="createdBy_name">'+ f.createdBy +' </p><p class="createdAt_date" id="date'+temp_id+'">'+ f.createdAt +'</p></li>');
     //     });
-
+    //     $.each(f.comment, function(i, f) {
+    //         $("#comment_ul").append('<li class="collection-item avatar"><img src="'+ img_icon +'" alt="" class="circle"><span class="creater_name">'+ f.comment +'</span><p class="created_data">'+ f.createdBy +' </br> '+ f.createdAt +'</p></li>');
+    //     });
     // });
+
+    $.each(userData, function (i, f) {
+        // console.log("userData", f);
+        var rating1 = {}
+        var temp_data = f
+        $.each(f.rating, function (i, f) {
+            // console.log("userData_function", f.rating);
+            rating1 = f.rating
+            // console.log("userData_function", f.memorable);
+            });
+            console.log("rating1",rating1);
+            
+            $("#card_container").append('<div class="card-content white-text"><div class="row"><div class="col s3" style="margin-top: 5px;"><i class="medium material-icons emoji brand-logo">'+ rating1 +'</i></div><div class="col s6"><h4 class="grey-text text-darken-4 title center-align card-title truncate">'+temp_data.title+'</h4></div><div class="col s3" style="margin-top: 10px;"><a href="#modal" id="modal_btn"class="secondary-content  btn-floating waves-effect waves-light modal-trigger"><i class="small material-icons">edit</i></a></div></div><div class="row"><div class="col s5"></div><div class="col s2"><img src="images/img_avatar.jpg" alt="" class="circle responsive-img"></div><div class="col s5"></div></div><div class="row valign-wrapper"><div class="col s2"><img src="images/img_avatar.jpg" alt="" class="circle responsive-img"></div><div class="col s8 center-align"><img src="images/img_avatar.jpg" alt="" class="circle responsive-img"></div><div class="col s2"><img src="images/img_avatar.jpg" alt="" class="circle responsive-img"></div></div><div class="row"><div class="col s5"></div><div class="col s2"><img src="images/img_avatar.jpg" alt="" class="circle responsive-img"></div><div class="col s5"></div></div><div class="row"><div class="card-tabs"><ul class="tabs tabs-fixed-width"><li class="tab"><a href="#memorable_div" class="blue white-text">Memorable</a></li><li class="tab"><a href="#comment_div" class="blue white-text">Comment</a></li></ul></div><div class="card-content black-text"><div id="memorable_div"><ul class="collection" id="memorable_ul"></ul></div><div id="comment_div"><ul class="collection" id="comment_ul"></ul><div class="input-field"><i class="material-icons prefix send_comment">mode_edit</i><textarea id="comment_txt" class="materialize-textarea"></textarea></div></div></div></div></div>')
+
+    });
 
     $.each(data.memorable, function (i, f) {
         $("#memorable_txt").text(f.memorable);
     });
 
-    $.each(data.rating, function (i, f) {
-        $(".emoji").text(f.rating);
-    });
+    // $.each(data.rating, function (i, f) {
+    //     $(".emoji").text(f.rating);
+    // });
 
     $("#modal_btn").click(function () {
         $('.modal').modal();
